e16cbebda84a3558bee3e60c255d4201fc572eda
s_daijiawen <s_daijiawen@wps.cn>
2024-07-02 14:52:20 +0800

    feat:完成了注册界面的开发

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,10 @@
 // 注册页面
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Register.css'; 
 
 const Register: React.FC = () => {
   const [username, setUsername] = useState('');
-  const [account, setAccount] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
@@ -13,28 +13,34 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="container">
-      <h2>Register</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Account"
-        value={account}
-        onChange={(e) => setAccount(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleRegister}>Register</button>
-      <button onClick={() => navigate('/')}>Back</button>
+    <div className="register-container">
+      <img src="/src/assets/WelcomeRegist.png" alt="欢迎注册" className="welcome-image" />
+      <div className="input-group">
+        <img src="/src/assets/Username.png" alt="用户名图标" />
+        <input
+          type="text"
+          placeholder="输入你的用户名"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <img src="/src/assets/PassWord.png" alt="密码图标" />
+        <input
+          type="password"
+          placeholder="输入你的密码"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="button-group">
+        <button onClick={() => navigate('/')}>
+          <img src="/src/assets/Exit.png" alt="退出" />
+        </button>
+        <button onClick={handleRegister}>
+          <img src="/src/assets/Regist.png" alt="注册" />
+        </button>
+      </div>
     </div>
   );
 };
